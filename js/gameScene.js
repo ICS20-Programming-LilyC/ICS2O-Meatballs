c1fa8b525d1b103954bda0378c6ccfb94802c3ab
Lilyyyc <124380100+Lilyyyc@users.noreply.github.com>
2023-06-08 01:13:06 +0000

    finished the coding, but start button won't work

--- js/gameScene.js
+++ js/gameScene.js
@@ -10,43 +10,82 @@
 class GameScene extends Phaser.Scene {
   // Constructor for the game scene.
   constructor() {
-    super({ key: "gameScene" });
+    super({ key: "gameScene" })
 
-    this.background = null;
-    this.cop = null;
-    this.firebullet = false;
+    this.background = null
+    this.cop = null
+    this.firebullet = false
+    this.score = 0
+    this.scoreText = null
+    this.scoreTextStyle = { font: "65px Georgia", fill: "#ffffff", align: "center" }
+
+    this.gameOverText = null
+    this.gameOverTextStyle = { font: "65px Georgia", fill: "ff0000", align: "center" }
   }
 
   // Initializing game scene.
   init(data) {
-    this.cameras.main.setBackgroundColor("669bbc");
+    this.cameras.main.setBackgroundColor("669bbc")
   }
 
   // Log game scene during preload phase.
   preload() {
-    console.log("Game Scene");
+    console.log("Game Scene")
 
-    //Images
-    this.load.image("cityBackground", "images/cityAndTornadeoBackground.png");
-    this.load.image("cop", "images/cop.png");
-    this.load.image("bullet", "images/bullet.png");
+    // Images
+    this.load.image("cityBackground", "images/cityAndTornadeoBackground.png")
+    this.load.image("cop", "images/cop.png")
+    this.load.image("bullet", "images/bullet.png")
+    this.load.image("meatball", "images/meatball.png")
 
-    //Sounds
-    this.load.audio("gun shot", "sounds/gunshot.wav")
+    // Sounds
+    this.sound.add("gun shot", "sounds/gunshot.wav")
+    this.sound.add("exploding", "sounds/exploding.wav")
+    this.sound.add("gameOver", "sounds/gameOver.wav")
   }
 
   // Creating data objects.
   create(data) {
-    this.background = this.add.sprite(0, 0, "cityBackground");
-    this.background.setOrigin(0, 0);
+    this.background = this.add.sprite(0, 0, "cityBackground")
+    this.background.setOrigin(0, 0)
+
+    // Setting up score to display to user.
+    this.scoreText = this.add.text(10, 10, "Score:" + this.score.toString(), this.scoreTextStyle)
 
     // Adjusting images to fit the user's screen. Link: https://photonstorm.github.io/phaser3-docs/Phaser.Cameras.Scene2D.Camera.html
-    this.background.setScale(this.cameras.main.width / this.background.width, this.cameras.main.height / this.background.height);
+    this.background.setScale(this.cameras.main.width / this.background.width, this.cameras.main.height / this.background.height)
 
-    this.cop = this.physics.add.sprite(1920 / 2, 1000 - 100, "cop");
+    this.cop = this.physics.add.sprite(1920 / 2, 1000 - 100, "cop")
 
     // Creating a group for the bullets
-    this.bulletGroup = this.physics.add.group();
+    this.bulletGroup = this.physics.add.group()
+
+    // Creating a group for the meatballs
+    this.meatballGroup = this.add.group()
+    this.createMeatball()
+
+    // Collisions set between bullets and meatballs
+    this.physics.add.collider(this.meatballGroup, this.bulletGroup, function (bulletCollide, meatballCollide) {
+      bulletCollide.destroy()
+      meatballCollide.destroy()
+      this.sound.play("exploding")
+      this.score += 1
+      this.scoreText.setText("Score: " + this.score.toString())
+      this.createMeatball()
+      this.createMeatball()
+    }.bind(this))
+
+    // Collisons between meatballs and cop
+    this.physics.add.collider(this.cop, this.bulletGroup, function (copCollide, meatballCollide) {
+      this.sound.play("gameOver")
+      this.physics.pause()
+      meatballCollide.destroy()
+      copCollide.destroy()
+        this.gameOverText = this.add.text(1920 / 2, 1080 / 2, "You Died. Game Over").setOrigin(0.5);
+
+      this.gameOverText.setInteractive({useHandCursor: true})
+      this.gameOverText.on("pointerdown", () => this.scene.start("gameScene"))
+    }.bind(this))
   }
 
   // Update the Game Scene.
@@ -102,6 +141,9 @@ class GameScene extends Phaser.Scene {
     //making missile upward
     this.bulletGroup.children.each(function (item) {
       item.y = item.y -15
+      if (item.y < 0) {
+        item.destroy()
+      }
     })
   }
 
