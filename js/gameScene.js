1ca0d3fcc0a76d3cedab7e563d7f43e4ecbc6073
Lilyyyc <124380100+Lilyyyc@users.noreply.github.com>
2023-06-12 15:50:44 +0000

    Bullets fire and cops work but meatballs don't work

--- js/gameScene.js
+++ js/gameScene.js
@@ -8,15 +8,32 @@
 
 // Adding a class to Game Scene.
 class GameScene extends Phaser.Scene {
-  // Constructor for the game scene.
 
+  //Create a meatball
+  createMeatball () {
+    // This will allow for a number between 1 and 1920.
+    const meatballXLocation = Math.floor(Math.random() * 1920) + 1
+    
+    // This will allow for a number between 1 and 50.
+    let meatballXVelocity = Math.floor(Math.random() * 50) + 1
+
+    //This will add a minus sign in 50% of the outcomes. 
+    meatballXVelocity *= Math.round(Math.random()) ? 1 : -1
+    const aMeatball = this.physics.add.sprite(meatballXLocation, -100,"meatball")
+    aMeatball.body.velocity.y = 200
+    aMeatball.body.velocity.x = meatballXVelocity
+    this.meatballGroup.add(aMeatball)
+  }
   
+// Constructor for the game scene.
   constructor() {
     super({ key: "gameScene" });
 
     this.background = null;
     this.cop = null;
     this.firebullet = false;
+    this.bulletGroup = null;
+    this.meatballGroup = null;
   }
 
   // Initializing game scene.
@@ -28,14 +45,14 @@ class GameScene extends Phaser.Scene {
   preload() {
     console.log("Game Scene");
 
-    //Images
+    // Images
     this.load.image("cityBackground", "images/cityAndTornadeoBackground.png");
     this.load.image("cop", "images/cop.png");
     this.load.image("bullet", "images/bullet.png");
     this.load.image("meatball", "images/meatball.png");
 
-    //Sounds
-    this.load.audio("gun shot", "sounds/gunshot.wav")
+    // Sounds
+    this.load.audio("gunshot", "sounds/gunshot.wav");
   }
 
   // Creating data objects.
@@ -43,72 +60,77 @@ class GameScene extends Phaser.Scene {
     this.background = this.add.sprite(0, 0, "cityBackground");
     this.background.setOrigin(0, 0);
 
-    // Adjusting images to fit the user's screen. Link: https://photonstorm.github.io/phaser3-docs/Phaser.Cameras.Scene2D.Camera.html
-    this.background.setScale(this.cameras.main.width / this.background.width, this.cameras.main.height / this.background.height);
+    // Adjusting images to fit the user's screen.
+    this.background.setScale(
+      this.cameras.main.width / this.background.width,
+      this.cameras.main.height / this.background.height
+    );
 
     this.cop = this.physics.add.sprite(1920 / 2, 1000 - 100, "cop");
 
-    // Creating a group for the bullets
+    // Creating a group for the bullets.
     this.bulletGroup = this.physics.add.group();
 
-    // Creating a group for the meatballs
-    this.meatballGroup = this.add.group();
-    this.createMeatball()
+    // Creating a group for the meatballs.
+    this.meatballGroup = this.physics.add.group();
+    this.createMeatball();
   }
 
   // Update the Game Scene.
   update(time, delta) {
-    
-    //Called 60 times in one second.
     const keyLeftObject = this.input.keyboard.addKey("LEFT");
     const keyRightObject = this.input.keyboard.addKey("RIGHT");
     const keySpaceObject = this.input.keyboard.addKey("SPACE");
 
     if (keyLeftObject.isDown === true) {
-
       // Move the cop sprite to the left by 15 units.
       this.cop.x -= 15;
       // If the cop sprite goes off the left edge of the screen, wrap it around to the right edge.
       if (this.cop.x < 0) {
-          this.cop.x = 1920;
-        }
+        this.cop.x = 1920;
+      }
     }
 
     if (keyRightObject.isDown === true) {
       // Move the cop sprite to the right by 15 units.
       this.cop.x += 15;
 
-      //If the cop sprite goes off the right edge of the screen, wrap it around to the left edge.
+      // If the cop sprite goes off the right edge of the screen, wrap it around to the left edge.
       if (this.cop.x > 1920) {
-          this.cop.x = 0;
-        }
+        this.cop.x = 0;
+      }
     }
 
     if (keySpaceObject.isDown === true) {
       if (this.firebullet === false) {
-        
         // Fire bullets
         // Set firebullet flag to true to prevent continuous firing.
-        this.firebullet = true
+        this.firebullet = true;
 
         // Create a new bullet sprite at a position slightly offset from the cop's position.
-        const aNewbullet = this.physics.add.sprite(this.cop.x - 15, this.cop.y - 235, "bullet");
+        const aNewbullet = this.physics.add.sprite(
+          this.cop.x - 15,
+          this.cop.y - 220,
+          "bullet"
+        );
 
         // Scale down the size of the bullet sprite.
-        aNewbullet.setScale(0.1); 
-        
+        aNewbullet.setScale(0.1);
         // Add the bullet sprite to the bullet group for further processing.
         this.bulletGroup.add(aNewbullet);
-        this.sound.play("gun shot");
+        this.sound.play("gunshot");
       }
+
     }
 
     if (keySpaceObject.isUp === true) {
       this.firebullet = false
     }
-    //making missile upward
+    // Making missile upward.
     this.bulletGroup.children.each(function (item) {
       item.y = item.y -15
+
+      // If the item (bullet) location is off the screen, then destroy it.
       if (item.y < 0) {
         item.destroy()
       }
--- js/titleScene.js
+++ js/titleScene.js
@@ -11,40 +11,39 @@ class TitleScene extends Phaser.Scene {
 
   // Constructor for the title scene.
   constructor() {
-    super({ key: "titleScene" })
+    super({ key: "titleScene" });
 
-    this.titleSceneBackgroundImage = null
-    this.titleSceneText = null
-    this.titleSceneTextStyle = { font: "48px Georgia", fill: "#003049", align: "center" }
+    this.titleSceneBackgroundImage = null;
+    this.titleSceneText = null;
+    this.titleSceneTextStyle = { font: "48px Georgia", fill: "#003049", align: "center" };
   }
 
   // Initializing title scene.
   init(data) {
-    this.cameras.main.setBackgroundColor("8d99ae")
+    this.cameras.main.setBackgroundColor("8d99ae");
   }
 
   // Log title scene during preload phase.
   preload() {
-    console.log("Title Scene")
-    this.load.image("titleSceneBackground", 'images/copVsMeatballsTitleScreen.png')
+    console.log("Title Scene");
+    this.load.image("titleSceneBackground", "images/copVsMeatballsTitleScreen.png");
   }
 
-// Creating data objects.
-create(data) {
-  this.titleSceneBackgroundImage = this.add.sprite(0, 0, "titleSceneBackground").setScale(2.75)
-  this.titleSceneBackgroundImage.x = 1920 / 2
-  this.titleSceneBackgroundImage.y = 1080 / 2
+  // Creating data objects.
+  create(data) {
+    this.titleSceneBackgroundImage = this.add.sprite(0, 0, "titleSceneBackground").setScale(2.75);
+    this.titleSceneBackgroundImage.x = 1920 / 2;
+    this.titleSceneBackgroundImage.y = 1080 / 2;
 
-  // Display a introduction message.
-  this.titleSceneText = this.add.text(1920 / 2, (1080 / 2) - 450, "Are you ready for...", this.titleSceneTextStyle).setOrigin(0.5)
-}
+    // Display an introduction message.
+    this.titleSceneText = this.add.text(1920 / 2, (1080 / 2) - 450, "Are you ready for...", this.titleSceneTextStyle).setOrigin(0.5);
+  }
 
   // Update the Title Scene.
-  update (time, delta) {
-  }
-  
+  update(time, delta) {
     if (time > 9000) {
-      this.scene.switch("menuScene")
+      this.scene.switch("menuScene");
+    }
   }
 }
 
