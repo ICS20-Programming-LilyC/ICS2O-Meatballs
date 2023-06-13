e8bc77f9249bf7cfb2263330f8c8d49598c1cace
Lilyyyc <124380100+Lilyyyc@users.noreply.github.com>
2023-06-13 18:02:06 +0000

    Added animation to splashScene.js and fixed text colors in instructionsScene.js

--- js/splashScene.js
+++ js/splashScene.js
@@ -30,11 +30,22 @@ class SplashScene extends Phaser.Scene {
     this.splashSceneBackgroundImage = this.add.sprite(0, 0, "splashSceneBackground")
     this.splashSceneBackgroundImage.x = 1920 / 2
     this.splashSceneBackgroundImage.y = 1080 / 2
+
+    // Adding in visual fade animation. Link: https://rexrainbow.github.io/phaser3-rex-notes/docs/site/tween/
+    this.tweens.add({
+      targets: this.splashSceneBackgroundImage,
+
+      // Using the alpha property since 0 = transparent, 1 = opaque.
+      alpha: 0,
+      
+      //Duration (in milliseconds).
+      duration: 4000,
+      });
   }
 
   // If statement to switch to title scene after a certain time.
   update(time, delta) {
-    if (time > 5000) {
+    if (time > 6000) {
       this.scene.switch("titleScene")
     }
   }
