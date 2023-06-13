bc642c83cf1003106eba13039bfcccda8104878d
Lilyyyc <124380100+Lilyyyc@users.noreply.github.com>
2023-06-13 03:16:32 +0000

    Got my instructions scene to work! (only 2 more extra functionality)

--- js/game.js
+++ js/game.js
@@ -7,16 +7,18 @@
 // This is the Phaser3 configuration file
 
 // Importing scene modules.
-import SplashScene from "./splashScene.js";
-import TitleScene from "./titleScene.js";
-import MenuScene from "./menuScene.js";
-import GameScene from "./gameScene.js";
+import SplashScene from "./splashScene.js"
+import TitleScene from "./titleScene.js"
+import MenuScene from "./menuScene.js"
+import GameScene from "./gameScene.js"
+import InstructionsScene from "./instructionsScene.js"
 
 // Creating instances of game scenes.
-const splashScene = new SplashScene();
-const titleScene = new TitleScene();
-const menuScene = new MenuScene();
-const gameScene = new GameScene();
+const splashScene = new SplashScene()
+const titleScene = new TitleScene()
+const menuScene = new MenuScene()
+const gameScene = new GameScene()
+const instructionsScene = new InstructionsScene()
 
 // Configuring constant for basic game settings.
 const config = {
@@ -40,10 +42,11 @@ const config = {
 const game = new Phaser.Game(config);
 
 // Adding scenes to the game.
-game.scene.add("splashScene", splashScene);
-game.scene.add("titleScene", titleScene);
-game.scene.add("menuScene", menuScene);
-game.scene.add("gameScene", gameScene);
+game.scene.add("splashScene", splashScene)
+game.scene.add("titleScene", titleScene)
+game.scene.add("menuScene", menuScene)
+game.scene.add("gameScene", gameScene)
+game.scene.add("instructionsScene", instructionsScene)
 
 // Starting the splash scene.
 game.scene.start("splashScene");
